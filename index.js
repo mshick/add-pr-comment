ad8b9da21964913d8ed2964d6f2ebaa8825acc13
Michael Shick <m@shick.us>
2019-11-25 13:51:39 -0500

    trigger

--- index.js
+++ index.js
@@ -5,23 +5,29 @@ async function run() {
   try {
     const msg = core.getInput("msg");
     const repoToken = core.getInput("repo-token");
+    const allowRepeats = core.getInput("allow-repeats");
+
+    console.log(
+      `allow repeats: ${allowRepeats}, typeof ${typeof allowRepeats}`
+    );
 
     core.debug(`Input message: ${msg}`);
 
     const octokit = new github.GitHub(repoToken);
     const {
       payload: {
+        after: commitSha,
         pull_request: pullRequestPayload,
         repository: repositoryPayload
       }
     } = github.context;
 
     const { number: pullNumber } = pullRequestPayload;
-    const { owner: repoOwner, full_name: repoFullName } = repositoryPayload;
+    const { full_name: repoFullName } = repositoryPayload;
     const [owner, repo] = repoFullName.split("/");
 
-    core.debug(`OWNER-------------------------------`);
-    console.log(repoOwner);
+    // core.debug(`OWNER-------------------------------`);
+    // console.log(repoOwner);
 
     const { data: pr } = await octokit.pulls.get({
       owner,
@@ -40,6 +46,23 @@ async function run() {
     });
 
     console.log(comments);
+
+    if (allowRepeats === false) {
+      core.debug(`NOT ALLOWING REPEATS, CHECK FOR DUPES`);
+    }
+
+    //   duplicate = coms.find { | c | c["user"]["login"] == "github-actions[bot]" && c["body"] == message }
+    //   if duplicate
+    // puts "The PR already contains a database change notification"
+    //   exit(0)
+    //   end
+
+    octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: pullNumber,
+      body: msg
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
