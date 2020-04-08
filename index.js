35f26ee693e25cdc5019365a66d020eb4d571053
Michael Shick <m@shick.us>
2020-04-08 16:45:36 -0700

    debug: adding context debugging

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ async function run() {
       payload: { pull_request: pullRequest, sha, repository },
     } = github.context;
 
+    core.debug(github.context);
+
     const { full_name: repoFullName } = repository;
 
     let issueNumber;
