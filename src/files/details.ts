2c74380443a9f3485dd46a9572b7601a0ba28a80
Michael Shick <m@shick.us>
2023-04-24 22:44:24 -0400

    try checks

--- src/files/details.ts
+++ src/files/details.ts
@@ -17,35 +17,49 @@ export interface WorkflowArtifactDetails {
 */
 export async function getWorkflowArtifactDetails(
   octokit: InstanceType<typeof GitHub>,
-): Promise<WorkflowArtifactDetails[]> {
-  const artifactDetails: WorkflowArtifactDetails[] = []
-  const payload = github.context.payload as any
-  core.info('context------------')
-  core.info(JSON.stringify(github.context, null, 2))
+  owner: string,
+  repo: string,
+): Promise<any> {
+  const result = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}/check-suites', {
+    owner,
+    repo,
+    ref: github.context.sha,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  })
 
-  core.info('payload------------')
-  core.info(JSON.stringify(payload, null, 2))
-  // const payload = github.context.payload as unknown as EventPayloadMap['workflow_run']
-  const workflowRun = payload.workflow_run
-  const repoHtmlUrl = payload.repository.html_url
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const checkSuiteNumber = workflowRun.check_suite_id!
-  const artifactsUrl = workflowRun.artifacts_url
-  const artifacts: ListWorkflowRunArtifacts['response']['data']['artifacts'] =
-    await octokit.paginate(artifactsUrl, {
-      per_page: 100,
-    })
+  core.info('result------------')
+  core.info(JSON.stringify(result, null, 2))
 
-  for (const artifact of artifacts) {
-    const artifactDetail = {
-      id: artifact.id,
-      name: artifact.name,
-      httpUrl: getArtifactUrl(repoHtmlUrl, checkSuiteNumber, artifact.id),
-    }
-    artifactDetails.push(artifactDetail)
-  }
+  // const artifactDetails: WorkflowArtifactDetails[] = []
+  // const payload = github.context.payload as any
+  // core.info('context------------')
+  // core.info(JSON.stringify(github.context, null, 2))
 
-  return artifactDetails
+  // core.info('payload------------')
+  // core.info(JSON.stringify(payload, null, 2))
+  // // const payload = github.context.payload as unknown as EventPayloadMap['workflow_run']
+  // const workflowRun = payload.workflow_run
+  // const repoHtmlUrl = payload.repository.html_url
+  // // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  // const checkSuiteNumber = workflowRun.check_suite_id!
+  // const artifactsUrl = workflowRun.artifacts_url
+  // const artifacts: ListWorkflowRunArtifacts['response']['data']['artifacts'] =
+  //   await octokit.paginate(artifactsUrl, {
+  //     per_page: 100,
+  //   })
+
+  // for (const artifact of artifacts) {
+  //   const artifactDetail = {
+  //     id: artifact.id,
+  //     name: artifact.name,
+  //     httpUrl: getArtifactUrl(repoHtmlUrl, checkSuiteNumber, artifact.id),
+  //   }
+  //   artifactDetails.push(artifactDetail)
+  // }
+
+  // return artifactDetails
 }
 
 // https://github.com/tonyhallett/DummyZipVsix/suites/2299172325/artifacts/48199605
